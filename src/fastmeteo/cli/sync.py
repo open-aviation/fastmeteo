df67594aa5e025aba137483aed696b49ba16a100
Junzi Sun <junzisun@gmail.com>
2025-06-12 00:12:31 +0200

    update sync logic

--- src/fastmeteo/cli/sync.py
+++ src/fastmeteo/cli/sync.py
@@ -6,8 +6,9 @@ from ..source import ArcoEra5
 
 @click.command()
 @click.option("--local-store", required="true", help="local era5 zarr store path")
+@click.option("--model-levels", default=37, help="levels 37 or 137")
 @click.option("--start", required="true", help="start datetime")
 @click.option("--stop", required="true", help="stop datetime")
-def main(local_store: str, start: str, stop: str) -> None:
-    fmg = ArcoEra5(local_store=local_store)
+def main(local_store: str, start: str, stop: str, model_levels: int) -> None:
+    fmg = ArcoEra5(local_store=local_store, model_levels=model_levels)
     fmg.sync_local(start, stop)
